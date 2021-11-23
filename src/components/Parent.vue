0ad67fb64500bafd90418ecfc6ce433b377f3b6a
Olya <olkaraduga7@gmail.com>
2021-11-23 13:00:42 +0300

    first version

---
+++ src/components/Parent.vue
@@ -0,0 +1,42 @@
+<template>
+    <div id="parent">
+        <a>
+
+        </a>
+        <div class='slider'>
+            <div class='image-slider'>
+                <img src='/images/2.jpg' />
+                <div class="block-front"><span>Турция</span></div>
+            </div>
+        </div>
+        <div class='good'>
+            <h1>
+                Лучшие направления
+            </h1>
+            <div class='better'>
+                <div class='image-better'>
+                    <img src='/images/92.jpg' />
+                </div>
+                <div class='image-better'>
+                    <img src='/images/11.jpg' />
+                </div>
+                <div class='image-better'>
+                    <img src='/images/12.jpg' />
+                </div>
+            </div>
+        </div>
+        <div class='fire'>
+            <div class='image-fire'>
+                <img src='/images/1bb5c28f10870397156d30851c229d31.jpg' />
+                <div class="block-front"><span>Горящие туры</span></div>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: "Parent"
+    }
+</script>
